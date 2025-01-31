5fbbaede0c32f0726093f315d9b4641d8eb895d0
iZergiodev <155020065+iZergiodev@users.noreply.github.com>
2025-01-31 01:51:53 +0000

    update

--- src/planetsservice.py
+++ src/planetsservice.py
@@ -8,27 +8,5 @@ class PlanetService:
             }
   
     def get_by_id(self, planet_id):
-        user = Planets.query.filter_by(id=planet_id).first()
-        return {
-            "message": "ok",
-            "result": {
-                "properties": {
-                "diameter": "10200",
-                "rotation_period": "24",
-                "orbital_period": "4818",
-                "gravity": "1 standard",
-                "population": "1000",
-                "climate": "temperate, tropical",
-                "terrain": "jungle, rainforests",
-                "surface_water": "8",
-                "created": "2025-01-30T12:15:53.709Z",
-                "edited": "2025-01-30T12:15:53.709Z",
-                "name": "Yavin IV",
-                "url": "https://www.swapi.tech/api/planets/3"
-                },
-                "description": "A planet.",
-                "_id": "5f7254c11b7dfa00041c6fb0",
-                "uid": "3",
-                "__v": 0
-            }
-            }
+        planet_by_id = Planets.query.filter_by(id=planet_id).first()
+        return planet_by_id.serialize()
